0a8500d6376bf0fd9f1838641e46955fb5ae479f
18subrato <subratoverma7@gmail.com>
2025-05-20 02:30:28 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todo,setTodo] = useState([]);
+  const [task,setTask] = useState('');
+  function handleSubmit(e){
+    e.preventDefault();
+    setTodo([...todo,task]);
+    setTask('');
+  }
+
+  function handleClick(id){
+    const updatedTodo = todo.filter((item)=>item != todo[id])
+    setTodo(updatedTodo);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={task} onChange={(e)=>setTask(e.target.value)} />
+          <br/>
+          <button type="submit">Add Todo</button>
+        </form>
+        <div>
+          {todo.map((item,index)=>(
+            <p key={index}>{item}<span><button onClick={()=>handleClick(index)}>Delete</button></span></p>
+          ))}
+        </div>
     </div>
   )
 }
